52bc07c2b25291dc41711915b5e09059c6b12dea
Alan Hong <xiupan@gmail.com>
2017-10-02 17:10:41 -0500

    Routes and Links all connected nicely in the nav bar.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
+import {BrowserRouter, Route} from 'react-router-dom'
 import BaseLayout from './BaseLayout'
 import CreatePost from './CreatePost'
 import PostList from './PostList'
@@ -10,19 +11,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        App
-        <BaseLayout>
-
-        </BaseLayout>
-        <CreatePost>
-
-        </CreatePost>
-        <PostList>
-
-        </PostList>
-        <ShowPost>
-
-        </ShowPost>
+        <BrowserRouter>
+          <div>
+            <BaseLayout>
+              <Route exact path="/" component={PostList}/>
+              <Route path="/create" component={CreatePost}/>
+              <Route path="/allposts" component={PostList}/>
+            </BaseLayout>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react'
+import {NavLink} from 'react-router-dom'
 
 class BaseLayout extends Component {
   render(){
     return(
       <div>
-        BaseLayout
+        <header>
+          <nav className="nav justify-content-center">
+            <NavLink exact to="/">
+            <li className="nav-link">
+              PollyBlog
+            </li>
+            </NavLink>
+            <NavLink to="/create">
+            <li className="nav-link">
+              Create Post
+            </li>
+            </NavLink>
+            <NavLink to="/allposts">
+            <li className="nav-link">
+              Show All Posts
+            </li>
+            </NavLink>
+          </nav>
+        </header>
+        {this.props.children}
       </div>
     )
   }
